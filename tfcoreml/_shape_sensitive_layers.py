a71013e280867391b2d564e168e8a119e55901b6
Yu-Cheng Ling <ycling@google.com>
2017-11-01 15:01:58 -0700

    Format more files according to PEP8

--- tfcoreml/_shape_sensitive_layers.py
+++ tfcoreml/_shape_sensitive_layers.py
@@ -1,6 +1,5 @@
 from tensorflow.python.util import compat
 import numpy as np
-from coremltools.models import *
 from _interpret_shapes import _interpret_shape as interpret_shape
 import _layers
 
@@ -11,7 +10,7 @@ def _remove_beginning_unit_dimensions(in_tuple):
     else:
       return in_tuple[i:]
 
-def _add_const(context, name, x, output_name, shape = None):
+def _add_const(context, name, x, output_name, shape=None):
   if output_name in context.load_constants_mlmodel:
     return
 
@@ -34,8 +33,8 @@ def _add_const(context, name, x, output_name, shape = None):
     x = np.reshape(x, (seq, h, w, c))
     #first check the simple case: seq. dimension is 1
     if seq == 1:
-      shape = [c, h, w] #(C,H,W)
-      x = np.transpose(x, [0,3,1,2])
+      shape = [c, h, w] # (C, H, W)
+      x = np.transpose(x, [0, 3, 1, 2])
       context.builder.add_load_constant(name, output_name, x, shape)
 
     #when sequence dimension is not 1, we need some permute layers as well
@@ -45,42 +44,42 @@ def _add_const(context, name, x, output_name, shape = None):
           'Add constant: cannot add a constant in which all the dimensions ' \
           '(Seq, C, H, W) are of non-unit size'
       if c == 1: #swap seq. and C
-        x = np.transpose(x, [3,0,1,2]) #(S,H,W,C) --> (C,S,H,W)
-        context.builder.add_load_constant(name, output_name + '_pre_permute',
-            x, [seq, h, w])
-        context.builder.add_permute(output_name, (1,0,2,3),
-            output_name + '_pre_permute', output_name)
+        x = np.transpose(x, [3, 0, 1, 2]) #(S,H,W,C) --> (C,S,H,W)
+        context.builder.add_load_constant(
+            name, output_name + '_pre_permute', x, [seq, h, w])
+        context.builder.add_permute(
+            output_name, (1, 0, 2, 3), output_name + '_pre_permute', output_name)
       elif h == 1: #swap seq. and H
-        x = np.transpose(x, [1,3,0,2]) #(S,H,W,C) --> (H,C,S,W)
-        context.builder.add_load_constant(name, output_name + '_pre_permute',
-            x, [c, seq, w])
-        context.builder.add_permute(output_name, (2,1,0,3),
-            output_name + '_pre_permute', output_name)
+        x = np.transpose(x, [1, 3, 0, 2]) #(S,H,W,C) --> (H,C,S,W)
+        context.builder.add_load_constant(
+            name, output_name + '_pre_permute', x, [c, seq, w])
+        context.builder.add_permute(
+            output_name, (2, 1, 0, 3), output_name + '_pre_permute', output_name)
       else: # w == 1, swap seq. and W
-        x = np.transpose(x, [2,3,1,0]) #(S,H,W,C) --> (W,C,H,S)
-        context.builder.add_load_constant(name, output_name + '_pre_permute',
-            x, [c, h, seq])
-        context.builder.add_permute(output_name, (3,1,2,0),
-            output_name + '_pre_permute', output_name)
+        x = np.transpose(x, [2, 3, 1, 0]) #(S,H,W,C) --> (W,C,H,S)
+        context.builder.add_load_constant(
+            name, output_name + '_pre_permute', x, [c, h, seq])
+        context.builder.add_permute(
+            output_name, (3, 1, 2, 0), output_name + '_pre_permute', output_name)
 
   else: #Static shape mapping
     shape = list(x.shape)
     assert len(shape) < 5, 'Const blob shape is more than rank 4'
     if len(shape) == 0:
-      shape = [1,1,1]      #(1,1,1)
+      shape = [1, 1, 1]      #(1,1,1)
     elif len(shape) == 1:
-      shape = [shape[0],1,1] #(C,1,1)
+      shape = [shape[0], 1, 1] #(C,1,1)
     elif len(shape) == 2:
       shape = [shape[1], 1, shape[0]] # HACK: (W,C) ---> (C,1,W) . Style transfer matrices are (W,C)
-      x = np.transpose(x,[1,0])
+      x = np.transpose(x, [1, 0])
     elif len(shape) == 3:
       shape = [shape[2], shape[0], shape[1]] # (H,W,C) ---> (C,H,W)
-      x = np.transpose(x,[2,0,1])
+      x = np.transpose(x, [2, 0, 1])
     elif len(shape) == 4:
       assert shape[0] == 1, 'Add Constant: Batch dimension must be 1'
       shape = [shape[3], shape[1], shape[2]]  #(B,H,W,C) ---> (C,H,W)
-      x = x[0,:,:,:] #(H,W,C)
-      x = np.transpose(x,[2,0,1])
+      x = x[0, :, :, :] #(H,W,C)
+      x = np.transpose(x, [2, 0, 1])
     context.builder.add_load_constant(name, output_name, x, shape)
 
 
@@ -92,7 +91,7 @@ def _add_concat(op, context):
 
   if op.type == 'Concat':
     axis_name = compat.as_bytes(op.inputs[0].name)
-    axis =  context.consts[axis_name]
+    axis = context.consts[axis_name]
     input_names = []
     for i, input in enumerate(op.inputs):
       if i == 0:
@@ -101,7 +100,7 @@ def _add_concat(op, context):
 
   if op.type == 'ConcatV2':
     axis_name = compat.as_bytes(op.inputs[-1].name)
-    axis =  context.consts[axis_name]
+    axis = context.consts[axis_name]
     input_names = []
     for i, input in enumerate(op.inputs):
       if i == len(op.inputs) - 1:
@@ -126,7 +125,7 @@ def _add_concat(op, context):
           'width dimensions'
   else:
     if len(output_shape) == 4:
-      assert axis in [1,2,3], 'Concat axis case not handled'
+      assert axis in [1, 2, 3], 'Concat axis case not handled'
     elif len(output_shape) == 3:
       axis += 1
     elif len(output_shape) == 1:
@@ -139,7 +138,7 @@ def _add_concat(op, context):
   # If concat's input is coming from another pool/concat: insert a linear activation layer
   coreml_layers = context.builder.nn_spec.layers
   for layer in coreml_layers:
-    if layer.WhichOneof('layer') in ['concat','pooling']:
+    if layer.WhichOneof('layer') in ['concat', 'pooling']:
       for i, inp in enumerate(input_names):
         if layer.output[0] == inp:
           out = inp + '__linear_activation'
@@ -147,28 +146,30 @@ def _add_concat(op, context):
           input_names[i] = out
 
   if axis == 3: #concatenate along channel axis
-    context.builder.add_elementwise(output_name, input_names, output_name,
-        'CONCAT')
+    context.builder.add_elementwise(
+        output_name, input_names, output_name, 'CONCAT')
   elif axis == 2: #concatentae along width axis
     inputs_permuted = []
     for i, input_name in enumerate(input_names):
-      context.builder.add_permute(output_name + '_' + str(i), (0,3,2,1),
+      context.builder.add_permute(
+          output_name + '_' + str(i), (0, 3, 2, 1),
           input_name, input_name + '_swap_W_C_' + str(i))
       inputs_permuted.append(input_name + '_swap_W_C_' + str(i))
-    context.builder.add_elementwise(output_name + '_concat', inputs_permuted,
-        output_name + '_concat', 'CONCAT')
-    context.builder.add_permute(output_name, (0,3,2,1),
-        output_name + '_concat', output_name)
+    context.builder.add_elementwise(
+        output_name + '_concat', inputs_permuted, output_name + '_concat', 'CONCAT')
+    context.builder.add_permute(
+        output_name, (0, 3, 2, 1), output_name + '_concat', output_name)
   elif axis == 1: #concatentae along heigth axis
     inputs_permuted = []
     for i, input_name in enumerate(input_names):
-      context.builder.add_permute(output_name + '_' + str(i), (0,2,1,3),
+      context.builder.add_permute(
+          output_name + '_' + str(i), (0, 2, 1, 3),
           input_name, input_name + '_swap_H_C_' + str(i))
       inputs_permuted.append(input_name + '_swap_H_C_' + str(i))
-    context.builder.add_elementwise(output_name + '_concat', inputs_permuted,
-        output_name + '_concat', 'CONCAT')
-    context.builder.add_permute(output_name, (0,2,1,3),
-        output_name + '_concat', output_name)
+    context.builder.add_elementwise(
+        output_name + '_concat', inputs_permuted, output_name + '_concat', 'CONCAT')
+    context.builder.add_permute(
+        output_name, (0, 2, 1, 3), output_name + '_concat', output_name)
   else:
     assert False, 'Concat axis case not handled'
   context.translated[output_name] = True
@@ -206,29 +207,29 @@ def _add_reshape(op, context):
   # TODO - reshaping just for mobilenet and stylenet:
   # if target_shape == (1,X) ----> new_shape = (X,1,1)
   # if targt_shape == (X,1) -----> new_shape = (1,1,X)
-  assert len(target_shape) in [2,3,4], \
-      'Reshape: Currently only supported if target shape is rank 2, 3 or 4'
+  assert len(target_shape) in [2, 3, 4], (
+      'Reshape: Currently only supported if target shape is rank 2, 3 or 4')
 
   mode = 0
   if len(target_shape) == 2:
     if target_shape[1] != 1: #(1,X)
-      new_shape = (1, target_shape[1],1,1)
+      new_shape = (1, target_shape[1], 1, 1)
       if len(input_shape) == 4 or len(input_shape) == 3:
         # (N,H,W,C) --> (1,C) or (N,S,C) --> (N,1,W,C)
         mode = 1
     else:
       new_shape = (1, 1, 1, target_shape[0])
-    context.builder.add_reshape(output_name, input_name, output_name,
-        new_shape, mode)
+    context.builder.add_reshape(
+        output_name, input_name, output_name, new_shape, mode)
   elif len(target_shape) == 3:
     new_shape = (1, target_shape[2], target_shape[0], target_shape[1])
-    context.builder.add_reshape(output_name, input_name, output_name,
-        new_shape, 1)
+    context.builder.add_reshape(
+        output_name, input_name, output_name, new_shape, 1)
   elif len(target_shape) == 4:
-    new_shape = (target_shape[0], target_shape[3], target_shape[1],
-        target_shape[2])
-    context.builder.add_reshape(output_name, input_name, output_name,
-        new_shape, 1)
+    new_shape = (
+        target_shape[0], target_shape[3], target_shape[1], target_shape[2])
+    context.builder.add_reshape(
+        output_name, input_name, output_name, new_shape, 1)
 
   context.translated[output_name] = True
 
@@ -240,7 +241,6 @@ def _add_reduce(op, context, mode):
   axis_ind = context.consts[op.inputs[1].name]
 
   input_shape = context.shape_dict[input_name]
-  output_shape = context.shape_dict[output_name]
 
   if context.use_dfs_shape_infer:
     status = interpret_shape(input_name, context)
@@ -259,8 +259,9 @@ def _add_reduce(op, context, mode):
       axis = ''.join(sorted(axis))
     else:
       axis = labeled_shape[axis_ind]
-    assert axis in ['S','C','H','W','CHW','HW'], \
-        ('Axis value %s not supported. Reduction supported along C, H, W, HW, CHW dimensions only.' %(axis))
+    assert axis in ['S', 'C', 'H', 'W', 'CHW', 'HW'], (
+        'Axis value %s not supported. '
+        'Reduction supported along C, H, W, HW, CHW dimensions only.' % axis)
   else:
     if isinstance(axis_ind, np.ndarray):
       axis_ind = axis_ind.tolist()
@@ -293,12 +294,13 @@ def _add_reduce(op, context, mode):
     context.builder.add_reduce(output_name, input_name, output_name, axis, mode)
   # Need to permute, reduce and then permute back
   else:
-    context.builder.add_permute(output_name, (1,0,2,3), input_name,
-        output_name + '_swap_Seq_C')
-    context.builder.add_reduce(output_name, output_name + '_swap_Seq_C',
+    context.builder.add_permute(
+        output_name, (1, 0, 2, 3), input_name, output_name + '_swap_Seq_C')
+    context.builder.add_reduce(
+        output_name, output_name + '_swap_Seq_C',
         output_name + '_pre_permute', 'C', mode)
-    context.builder.add_permute(output_name, (1,0,2,3),
-        output_name + '_pre_permute', output_name)
+    context.builder.add_permute(
+        output_name, (1, 0, 2, 3), output_name + '_pre_permute', output_name)
   context.translated[output_name] = True
 
 def _add_mean(op, context):
@@ -308,7 +310,6 @@ def _add_mean(op, context):
   axis_ind = context.consts[op.inputs[1].name]
 
   input_shape = context.shape_dict[input_name]
-  output_shape = context.shape_dict[output_name]
 
   if context.use_dfs_shape_infer:
     status = interpret_shape(input_name, context)
@@ -325,11 +326,13 @@ def _add_mean(op, context):
       axis = ''.join(sorted(axis))
     else:
       axis = labeled_shape[axis_ind]
-    assert axis in ['S','C','H','W','CHW','HW'], \
-        ('Axis value %s not supported. Reduction supported along C, H, W, HW, CHW dimensions only.' %(axis))
+    assert axis in ['S', 'C', 'H', 'W', 'CHW', 'HW'], (
+        'Axis value %s not supported. '
+        'Reduction supported along C, H, W, HW, CHW dimensions only.' % axis)
   else:
-    if len(input_shape) == 4 and (np.array_equal(axis_ind, np.array([0,1,2])) \
-        or np.array_equal(axis_ind, np.array([1,2]))):
+    if len(input_shape) == 4 and (
+        np.array_equal(axis_ind, np.array([0, 1, 2])) or
+        np.array_equal(axis_ind, np.array([1, 2]))):
       axis = 'HW'
     else:
       assert False, 'Mean axis case not handled currently'
@@ -340,11 +343,11 @@ def _add_mean(op, context):
     context.builder.add_reduce(output_name, input_name, output_name, axis, mode)
   # Need to permute, reduce and then permute back
   else:
-    context.builder.add_permute(output_name, (1,0,2,3), input_name,
-        output_name + '_swap_Seq_C')
-    context.builder.add_reduce(output_name, output_name + '_swap_Seq_C',
+    context.builder.add_permute(
+        output_name, (1, 0, 2, 3), input_name, output_name + '_swap_Seq_C')
+    context.builder.add_reduce(
+        output_name, output_name + '_swap_Seq_C',
         output_name + '_pre_permute', 'C', mode)
-    context.builder.add_permute(output_name, (1,0,2,3),
-        output_name + '_pre_permute', output_name)
+    context.builder.add_permute(
+        output_name, (1, 0, 2, 3), output_name + '_pre_permute', output_name)
   context.translated[output_name] = True
-
