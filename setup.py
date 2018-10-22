bc3b5c75a4eaa9b4ca0cf4bf8304cb186b938142
Aseem Wadhwa <aseem.elec@gmail.com>
2018-10-22 16:00:59 -0700

    update version to 0.3.0 (#255)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(README) as f:
 
 setup(
     name='tfcoreml',
-    version='0.2.0',
+    version='0.3.0',
     description='Tensorflow to Core ML converter',
     long_description=_LONG_DESCRIPTION,
     url='',
