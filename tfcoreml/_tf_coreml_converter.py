e43c243439b673773bd1375768f5770328362f1f
Bhushan Sonawane <bhushansonawaneint@Bhushans-MacBook-Pro-Int.local>
2019-09-16 16:12:34 -0700

    Adding image input flags for new converter path

--- tfcoreml/_tf_coreml_converter.py
+++ tfcoreml/_tf_coreml_converter.py
@@ -294,6 +294,7 @@ def _convert_pb_to_mlmodel(tf_model_path,
     _check_unsupported_ops(OPS, output_feature_names, effectively_constant_ops + unused_ops)
   print('Now starting translation to CoreML graph.')
 
+
   # Load all the dictionaries in the object of the class "context"
   context = Context(CONSTS, SHAPE_DICT, OPS, BLOB_GRAPH, output_features)
 
@@ -476,7 +477,8 @@ def convert(tf_model_path,
             predicted_probabilities_output='',
             add_custom_layers=False,  # type: bool
             custom_conversion_functions={},  # type: Dict[Text, Any]
-            use_coreml_3=False
+            custom_shape_functions={}, # type: Dict[Text, Any]
+            use_coreml_3=False,
             ):
 
   """
@@ -502,9 +504,7 @@ def convert(tf_model_path,
       that can be treated as images by Core ML. All other inputs
       are treated as MultiArrays.
 
-  is_bgr: bool | dict()
-      Flag to determine if input images are in pixel order (RGB or BGR).
-      Defaults to False.
+  is_bgr: bool | dict():
       Applicable only if image_input_names is specified.
       To specify different values for each image input provide a dictionary with input names as keys.    
 
@@ -568,9 +568,18 @@ def convert(tf_model_path,
       dictionary containing the op's inputs that are constants and their values (as numpy arrays).
       The function can add custom layers or any other combination of CoreML layers to translate the TF op. 
       See "examples/custom_layer_examples.ipynb" jupyter-notebook for examples on using this argument. 
+    
+  custom_shape_functions: dict(): {Text: func()}
+      Argument to provide user-defined functions to compute shape for given op.
+      A dictionary with keys corresponding to the type of TF Op and value as hadnled to user-defined function.
+      Function receives `layer specification` and `input shape` as a input.
+      output of the function must be output shape for give op. (generally List).
+      Custom shape function is required for adding custom layer in Core ML 3.
+      If use_coreml_3 is False, this functions are ignored
 
   use_coreml_3: bool
       Flag to switch to coremltools.converters.tensorflow which uses new Core ML 3 features.
+    
   Returns
   -------
   model: MLModel
@@ -579,19 +588,23 @@ def convert(tf_model_path,
   """
 
   if use_coreml_3:
-    if image_input_names is not None:
-      raise NotImplementedError('Core ML 3 converter does not support image_input_names argument yet.')
-    if not (is_bgr == False and red_bias == 0.0 and green_bias == 0.0 and blue_bias == 0.0 and gray_bias == 0.0 and image_scale == 1.0):
-      raise NotImplementedError('Core ML 3 converter does not support image-related preprocessing arguments yet.')
-    if not (class_labels is None and predicted_feature_name is None and predicted_probabilities_output == ''):
-      raise NotImplementedError('Core ML 3 converter does not support NeuralNetworkClassifier interface yet.')
-    if not (add_custom_layers == False):
-      raise NotImplementedError('Core ML 3 converter does not support Custom layers yet.')
-
     mlmodel = coremltools.converters.tensorflow.convert(
-        tf_model_path,
-        inputs=input_name_shape_dict,
-        outputs=output_feature_names)
+                  tf_model_path,
+                  inputs=input_name_shape_dict,
+                  outputs=output_feature_names,
+                  image_input_names=image_input_names,
+                  is_bgr=is_bgr,
+                  red_bias=red_bias,
+                  green_bias=green_bias,
+                  blue_bias=blue_bias,
+                  gray_bias=gray_bias,
+                  image_scale=image_scale,
+                  class_labels=class_labels,
+                  predicted_feature_name=predicted_feature_name,
+                  predicted_probabilities_output=predicted_probabilities_output,
+                  add_custom_layers=add_custom_layers,
+                  custom_conversion_functions=custom_conversion_functions,
+                  custom_shape_functions=custom_shape_functions)
     if mlmodel_path is not None:
       mlmodel.save(mlmodel_path)
     return mlmodel
